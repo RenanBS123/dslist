b1210078fc8f51f68b64d476cf4add2a141bd10a
renan silveira <rs878710@gmail.com>
2023-05-11 20:34:17 -0300

    GET lists

---
+++ src/main/java/com/renan/dslist/controllers/GameListController.java
@@ -0,0 +1,26 @@
+package com.renan.dslist.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.renan.dslist.dto.GameListDTO;
+import com.renan.dslist.services.GameListService;
+
+@RestController
+@RequestMapping(value = "/lists")
+public class GameListController {
+	
+	@Autowired
+	private GameListService gameListService;
+	
+	@GetMapping
+	public List<GameListDTO> findall(){
+		List<GameListDTO> result = gameListService.findAll();
+		return result;
+
+	}
+}
